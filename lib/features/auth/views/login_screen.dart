4cce9f9b8a1d80ac260a9970d7aa3e3ff179748d
mayurpawar17 <mayurpawar4908@gmail.com>
2025-12-29 00:35:04 +0530

    ui update in login screen

--- lib/features/auth/views/login_screen.dart
+++ lib/features/auth/views/login_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:inprompt_ai_flutter/features/auth/widgets/google_sign_in_button.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final emailEditingController = TextEditingController();
+  final passwordEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center(child: Text('Login Screen'))),
+      backgroundColor: Colors.white,
+      // backgroundColor: AppColors.primaryBgColor,
+      // backgroundColor: AppColors.primary,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(child:GoogleSignInButton(onTap: (){})),
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/auth/widgets/google_sign_in_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const GoogleSignInButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(4),
+          border: Border.all(color: Colors.grey.shade300, width: 1),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/google.png', width: 30, height: 30),
+            SizedBox(width: 12),
+            Text(
+              'Sign in with Google',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
